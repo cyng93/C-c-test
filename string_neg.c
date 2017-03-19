99e37443bd93d723bb8fa2ea25d6081c9370f95e
cyng93 <cyng93+github@gmail.com>
2017-03-19 21:25:36 +0800

    Add negative test cases for string

--- string_neg.c
+++ string_neg.c
@@ -1,2 +1,4 @@
-string need to start with double-quote"
-"string need to end with double-quote
+string have to start with double-quote"
+"string have to end with double-quote
+"string cannot end with backslash\
+"string cannot end with double-quote that were escaped\"
